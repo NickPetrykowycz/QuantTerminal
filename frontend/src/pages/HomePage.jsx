63f399b26c8d2e19848749c533980ebcb4bd14de
Nicholas Petrykowycz <z5359619@ad.unsw.edu.au>
2025-06-03 22:47:24 +1000

    added some icons for ideas

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import BorderContainerStatic from '../components/BorderContainer';
 import FlexibleIconButton from '../components/FlexibleIconButton';
 import { IoFolderOpenOutline } from "react-icons/io5";
+import { PiToolbox, PiVault, PiJoystick} from "react-icons/pi";
+import { BsJoystick } from "react-icons/bs";
 
 function HomePage() {
   return (
@@ -10,9 +12,24 @@ function HomePage() {
         <div className="w-full h-full flex flex-col items-center justify-center">
           <div className="flex flex-wrap justify-center mt-8">
             <FlexibleIconButton
-              icon={<IoFolderOpenOutline />}
-              label="Quant Tools"
+              icon={<PiToolbox />}
+              label="The Toolbox"
               to="/quant-tools"
+            />
+            <FlexibleIconButton
+              icon={<IoFolderOpenOutline />}
+              label="The Archive"
+              to="/quant-notes"
+            />
+            <FlexibleIconButton
+              icon={<PiVault />}
+              label="The Vault"
+              to="/quant-projects"
+            />
+            <FlexibleIconButton
+              icon={<PiJoystick />}
+              label="The Arcade"
+              to="/quant-games"
             />
             </div>
         </div>
