a51115d2e2b68c3432ac73c0b504ecc531dc66c2
Nicholas Petrykowycz <z5359619@ad.unsw.edu.au>
2025-07-12 13:01:06 +1000

    Finished European Options Info (For Now)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import LearningCenter from './pages/LearningCenter';
 import OptionsHub from './pages/OptionsHub';
 import OptionsOverview from './pages/OptionsOverview';
 import AmericanOptions from './pages/AmericanOptions';
+import EuropeanOptions from './pages/EuropeanOptions';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/learning/options" element={<OptionsHub />} />
           <Route path="/learning/options/overview" element={<OptionsOverview />} />
           <Route path="/learning/options/american" element={<AmericanOptions />} />
+          <Route path="/learning/options/european" element={<EuropeanOptions />} />
           <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </Router>
